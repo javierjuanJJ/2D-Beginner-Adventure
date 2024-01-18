af2c9c04539c4c66d6242d0fa9058ac27f79d4a9
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2024-01-18 12:30:15 +0100

    Commit 3 Create input actions for player character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,47 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float speed;
-    
+    public InputAction MoveAction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();  
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            position.x += speed;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            position.y += speed;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            position.x -= speed;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            position.y -= speed;
-        }
-        if (Input.GetKeyDown(KeyCode.Z) && speed > 0)
-        {
-            speed += 1;
-        }
-        if (Input.GetKeyDown(KeyCode.Y) && speed > 1 )
-        {
-            speed -= 1;
-        }
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
