aff519ff75b70182f16ded95c30e751cd35ea74b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2024-02-08 16:09:40 +0100

    Create 2D sprite animations

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    /// Public variables
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+    [SerializeField]
+    private int puntuation;
+  
+    // Private variables
+    Rigidbody2D rigidbody2d;
+    float timer;
+    int direction = 1;
+
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = changeTime;
+    }
+
+
+    // Update is called every frame
+    void Update()
+    {
+        timer-= Time.deltaTime;
+
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+
+
+
+    // FixedUpdate has the same call rate as the physics system
+    void FixedUpdate()
+    {    
+        Vector2 position = rigidbody2d.position;
+        
+        if (vertical)
+        {
+            position.y += speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            position.x += speed * direction * Time.deltaTime;
+        }
+
+
+        rigidbody2d.MovePosition(position);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+
+        if (other.CompareTag("Player"))
+        {
+            player.ChangeHealth(puntuation);
+        }
+    }
+
+}
